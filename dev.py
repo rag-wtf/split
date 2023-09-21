388ce5321bf26978c4f429f95eb36f76a78e498d
Lim Chee Kin <limcheekin@vobject.com>
2023-09-21 19:23:18 +0800

    feat: added local dev server run with .env and dev.sh to upload file, reduced txt file size and updated test lambda step of github workflow

---
+++ dev.py
@@ -0,0 +1,10 @@
+import os
+from load_split_embed import create_app
+from dotenv import load_dotenv
+load_dotenv()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        create_app(), host=os.getenv("HOST", "localhost"), port=int(os.getenv("PORT", 8000))
+    )
---
+++ dev.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: multipart/form-data" -F "file=@breathing.txt" http://localhost:8000/load_split_embed
