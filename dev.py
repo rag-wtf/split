7def3f54b01becab818a3dc626e7eff08adb03d8
limcheekin <limcheekin@vobject.com>
2025-07-31 14:47:20 +0800

    chore: remove unnecessary code

--- dev.py
+++
@@ -1,9 +0,0 @@
-from split import create_app, settings # Import settings
-from dotenv import load_dotenv
-load_dotenv() # pydantic-settings will also try to load .env, but this ensures it's loaded early
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        create_app(), host=settings.host, port=settings.port
-    )
